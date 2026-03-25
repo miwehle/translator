0447aabc5928f3229aca57a465b99f03d3eeac00
Michael Wehle <michael.wehle@mt-ag.com>
2026-03-25 01:24:04 +0100

    außen Ungenutztes aus __init__.py Dateien entfernt

--- src/translator/inference/__init__.py
+++ src/translator/inference/__init__.py
@@ -1,13 +1,7 @@
 from .preview_translation import (
     create_translation_preview_fn,
 )
-from .tokenizer import (
-    HuggingFaceTokenizerAdapter,
-    TokenizerProtocol,
-)
 
 __all__ = [
     "create_translation_preview_fn",
-    "HuggingFaceTokenizerAdapter",
-    "TokenizerProtocol",
 ]
--- src/translator/shared/__init__.py
+++ src/translator/shared/__init__.py
@@ -1,12 +1,10 @@
 from .logging_utils import (
-    close_translator_logging,
     configure_translator_logging,
     detect_hardware_type,
 )
 from .types import Example
 
 __all__ = [
-    "close_translator_logging",
     "configure_translator_logging",
     "detect_hardware_type",
     "Example",
