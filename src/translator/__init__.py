39846c0845dc41bdad43645087691d2b64ff45f8
Michael Wehle <michael.wehle@mt-ag.com>
2026-04-04 15:46:15 +0200

    add Translator to translator package API

--- src/translator/__init__.py
+++ src/translator/__init__.py
@@ -1,9 +1,11 @@
 from .api import check_dataset, train
+from .inference import Translator
 from .training import DataLoaderConfig, ModelConfig, TrainConfig, TrainingSummary
 
 __all__ = [
     "train",
     "check_dataset",
+    "Translator",
     "DataLoaderConfig",
     "ModelConfig",
     "TrainConfig",
